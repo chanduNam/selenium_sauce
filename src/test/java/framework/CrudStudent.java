f396c6cf198df3bf8a804b0768b97928edb09b93
chanduNam <45768343+chanduNam@users.noreply.github.com>
2019-01-15 12:17:52 +0530

    Update CrudStudent.java

--- src/test/java/framework/CrudStudent.java
+++ src/test/java/framework/CrudStudent.java
@@ -24,7 +24,7 @@ public class CrudStudent extends DriverFactory {
 		
 		driver=DriverFactory.getDriver();
 		driver.manage().window().maximize();
-		driver.get("http://34.73.172.246/student/");
+		driver.get("http://34.73.192.143/student/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
